0afa5e3a70228a35a8daacdde2bfef5495278c64
abc7559582 <2495678470@qq.com>
2022-10-31 00:15:31 +0800

    第八次

--- Assets/gfdfg.cs
+++ Assets/gfdfg.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class eeee : MonoBehaviour
+public class gfdfg : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
