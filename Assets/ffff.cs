45757a1836e74fbb3fbe5af7612875a1aedc8476
abc7559582 <2495678470@qq.com>
2022-10-31 00:10:06 +0800

    第六次

--- Assets/ffff.cs
+++ Assets/ffff.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cccc : MonoBehaviour
+public class ffff : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
